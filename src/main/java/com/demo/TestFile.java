9c46aa92698c8a14fc9fe14a32e74d0cc307eef3
parkjisong <pjs96525@hanmail.net>
2025-08-03 00:05:04 +0900

    feat:user 엔티티 생성

--- src/main/java/com/demo/TestFile.java
+++ src/main/java/com/demo/TestFile.java
@@ -1,4 +1,4 @@
-package com.test;
+package com.demo;
 
 public class TestFile {
 }
--- src/main/java/com/demo/config/CorsConfig.java
+++ src/main/java/com/demo/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.test.config;
+package com.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
